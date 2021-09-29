dd00cda08b683cb34f59859dd1f6cc6353cb2030
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-09-29 19:42:35 -0300

    Enemy & AssaultEnemy

---
+++ Assets/Scripts/AssaultEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssaultEnemy : Enemy
+{
+    private Animator _myAnimator;
+
+    private void Start()
+    {
+        _myAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Patrol();
+    }
+
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    public List<Transform> waypoints;
+    public float speed;
+    private int _currentWaypoint = 0;
+
+    public void Patrol()
+    {
+        Vector3 dir = waypoints[_currentWaypoint].position - transform.position;
+        transform.forward = dir;
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        if (dir.magnitude < 0.1f)
+        {
+            _currentWaypoint++;
+            if (_currentWaypoint > waypoints.Count - 1)
+                _currentWaypoint = 0;
+        }
+    }
+     
+}
