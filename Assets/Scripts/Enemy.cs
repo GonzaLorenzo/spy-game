e96c24b9a26d3069319a9276426cd3e122c5ee19
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-09-29 19:50:29 -0300

    Enemy frena y se hidrata

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,19 +7,39 @@ public abstract class Enemy : MonoBehaviour
     public List<Transform> waypoints;
     public float speed;
     private int _currentWaypoint = 0;
+    private bool canMove = true;
 
     public void Patrol()
     {
-        Vector3 dir = waypoints[_currentWaypoint].position - transform.position;
-        transform.forward = dir;
-        transform.position += transform.forward * speed * Time.deltaTime;
-
-        if (dir.magnitude < 0.1f)
+        if (canMove)
         {
-            _currentWaypoint++;
-            if (_currentWaypoint > waypoints.Count - 1)
-                _currentWaypoint = 0;
+            Vector3 dir = waypoints[_currentWaypoint].position - transform.position;
+            transform.forward = dir;
+            transform.position += transform.forward * speed * Time.deltaTime;
+
+            if (dir.magnitude < 0.1f)
+            {
+                AwaitInPlace(2f);
+                _currentWaypoint++;
+                if (_currentWaypoint > waypoints.Count - 1)
+                    _currentWaypoint = 0;
+            }
         }
     }
-     
+
+    private void AwaitInPlace(float time)
+    {
+        StartCoroutine(LockMovementFor(time));
+    }
+    private IEnumerator LockMovementFor(float time)
+    {
+        canMove = false;
+        //SoldadoState = 1f;
+        //SetSoldadoState();
+        yield return new WaitForSeconds(time);
+        canMove = true;
+        //SoldadoState = 2f;
+        //SetSoldadoState();
+
+    }
 }
