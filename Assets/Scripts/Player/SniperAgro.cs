5676cf6610e94d4f7e45f8207556da9710d08bb5
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-10-01 20:12:19 -0300

    Notas

--- Assets/Scripts/Player/SniperAgro.cs
+++ Assets/Scripts/Player/SniperAgro.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class SniperAgro : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    //Osea, si el enemigo o distracción que tengo tiene la interfaz IShootable lo marco con la UI
+    //Y con el ActionButton procedo a activarle el Shoot() que mataría el enemigo o activaría la distracción.
+
+    private GameObject selectedEnemy;
+
+    private void OnTriggerEnter(Enemy other)
     {
-        
+        //if (other.Shoot() != null)
     }
 }
