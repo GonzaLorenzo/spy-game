810b47035b8a68fbed72f595b59bcfb27520a6d7
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-10-01 20:35:52 -0300

    Falta la UI de sniper

--- Assets/Scripts/Player/SniperAgro.cs
+++ Assets/Scripts/Player/SniperAgro.cs
@@ -7,10 +7,22 @@ public class SniperAgro : MonoBehaviour
     //Osea, si el enemigo o distracción que tengo tiene la interfaz IShootable lo marco con la UI
     //Y con el ActionButton procedo a activarle el Shoot() que mataría el enemigo o activaría la distracción.
 
-    private GameObject selectedEnemy;
+    //other.GetComponent<IShootable>().Shoot();
 
-    private void OnTriggerEnter(Enemy other)
+    private GameObject selectedEnemy;
+    private GameObject sniperUI;
+
+    private void OnTriggerEnter(Collider other)
     {
-        //if (other.Shoot() != null)
+        if (other.GetComponent<IShootable>() != null)
+        {
+            //Vector3 pos = ;
+            Instantiate(sniperUI, other.transform.position);
+        }
+    }
+
+    public void ShootTarget()
+    {
+        selectedEnemy.GetComponent<IShootable>().Shoot();
     }
 }
