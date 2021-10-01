8d53700d29e31b154b1868d08ae3e7862cd925b6
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-10-01 20:04:34 -0300

    IShootable
    El Sniper huele el IShootable

--- Assets/Scripts/Enemy/AssaultEnemy.cs
+++ Assets/Scripts/Enemy/AssaultEnemy.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AssaultEnemy : Enemy
+public class AssaultEnemy : Enemy, IShootable
 {
     private Animator _myAnimator;
+
+    public void Shoot()
+    {
+        //Animator de muerte
+        //Dejar de Patrullar y capaz instanciar particulas de sangre
+    }
 
     private void Start()
     {
