a67259c69b94f665aa45edc8a73ad256ca47be6d
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-10-05 19:56:07 -0300

    saun saun

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -1,77 +1,122 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+    /// <summary>
+    /// Generates and updates the vision cone displayed
+    /// </summary>
 
-public class FieldOfView : MonoBehaviour
-{
-    private Mesh mesh;
-
-    private void Start()
+    public class FieldOfView : MonoBehaviour
     {
-        mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
-    }
+        private Mesh mesh;
+        public float vision_angle = 30f;
+        public float vision_range = 5f;
+        public float vision_near_range = 3f;
+        public LayerMask obstacle_mask = ~(0);
+        public bool show_two_levels = false;
 
-    private void Update()
-    {
-        float fov = 60f;
-        Vector3 origin = Vector3.zero;
-        int rayCount = 50;
-        float angle = 0f;
-        float angleIncrease = fov / rayCount;
-        float viewDistance = 5f;
+        public int precision = 60;
 
-        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
-        Vector2[] uv = new Vector2[vertices.Length];
-        int[] triangles = new int[rayCount * 3];
+        private float timer = 0f;
+        private float refresh_rate = 0f;
 
-        vertices[0] = origin;
-
-        int vertexIndex = 1;
-        int triangleIndex = 0;
-
-        for (int i = 0; i <= rayCount; i++)
+        private void Start()
         {
-            Vector3 vertex; //= origin + GetVectorFromAngle(angle) * viewDistance;
-
-            Ray enemyRay = new Ray(origin, GetVectorFromAngle(angle));
-            RaycastHit hit;
-            Physics.Raycast(enemyRay, out hit, viewDistance);
-
-            if (hit.collider == null)
-            {
-                vertex = origin + GetVectorFromAngle(angle) * viewDistance;
-            }
-            else
-            {
-                Debug.Log("Hitting: " + hit.point);
-                vertex = GetVectorFromAngle(angle).normalized * hit.distance;
-            }
-
-            vertices[vertexIndex] = vertex;
-
-            if (i > 0)
-            {
-                triangles[triangleIndex + 0] = 0;
-                triangles[triangleIndex + 1] = vertexIndex - 1;
-                triangles[triangleIndex + 2] = vertexIndex;
-
-                triangleIndex += 3;
-            }
-
-            vertexIndex++;
-            angle -= angleIncrease;
-
+            mesh = new Mesh();
+            GetComponent<MeshFilter>().mesh = mesh;
+            //InitMesh(mesh);
         }
 
-        mesh.vertices = vertices;
-        mesh.uv = uv;
-        mesh.triangles = triangles;
+        private void InitMesh()//MeshFilter. mesh)
+        {
+            List<Vector3> vertices = new List<Vector3>();
+            List<int> triangles = new List<int>();
+            List<Vector3> normals = new List<Vector3>();
+            List<Vector2> uv = new List<Vector2>();
+
+            int minmax = Mathf.RoundToInt(vision_angle / 2f);
+
+            int tri_index = 0;
+            float step_jump = Mathf.Clamp(vision_angle / precision, 0.01f, minmax);
+
+            for (float i = -minmax; i <= minmax; i += step_jump)
+            {
+                float angle = (float)(i + 90f) * Mathf.Deg2Rad;
+                Vector3 dir = new Vector3(Mathf.Cos(angle) * vision_range, 0f, Mathf.Sin(angle) * vision_range);
+
+                vertices.Add(dir);
+                normals.Add(Vector2.up);
+                uv.Add(Vector2.zero);
+
+                if (tri_index > 0)
+                {
+
+                    triangles.Add(0);
+                    triangles.Add(tri_index + 1);
+                    triangles.Add(tri_index);
+
+
+                }
+            }
+
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles.ToArray();
+            mesh.normals = normals.ToArray();
+            mesh.uv = uv.ToArray();
+        }
+
+    //private void Update()
+    //{
+    //timer += Time.deltaTime;
+
+    //transform.position = target.eye.transform.position;
+    //transform.rotation = target.transform.rotation;
+
+    //if (timer > refresh_rate)
+    //{
+    //    timer = 0f;
+
+    //    float range = vision_range;
+    //    if (show_two_levels)
+    //        range = vision_near_range;
+    //
+    //    UpdateMainLevel(mesh, range);
+    //}
+    //}
+
+        private void Update()
+        {
+
+            //private void UpdateMainLevel(MeshFilter mesh, float range)
+            //{
+
+
+
+
+            List<Vector3> vertices = new List<Vector3>();
+            vertices.Add(new Vector3(0f, 0f, 0f));
+
+            int minmax = Mathf.RoundToInt(vision_angle / 2f);
+            float step_jump = Mathf.Clamp(vision_angle / precision, 0.01f, minmax);
+            for (float i = -minmax; i <= minmax; i += step_jump)
+            {
+            float angle = (float)(i + 90f) * Mathf.Deg2Rad;
+            Vector3 dir = new Vector3(Mathf.Cos(angle) * vision_range, 0f, Mathf.Sin(angle) * vision_range);
+
+            RaycastHit hit;
+            Vector3 pos_world = transform.TransformPoint(Vector3.zero);
+            Vector3 dir_world = transform.TransformDirection(dir.normalized);
+            bool ishit = Physics.Raycast(new Ray(pos_world, dir_world), out hit, vision_range, obstacle_mask.value);
+            if (ishit)
+                dir = dir.normalized * hit.distance;
+            Debug.DrawRay(pos_world, dir_world * (ishit ? hit.distance : vision_range));
+
+            vertices.Add(dir);
+            }
+
+            mesh.vertices = vertices.ToArray();
+            mesh.RecalculateBounds();
+            //}
+        }
+
     }
 
-    public static Vector3 GetVectorFromAngle(float angle)
-    {
-        float angleRad = angle * (Mathf.PI / 180f);
-        return new Vector3(Mathf.Cos(angleRad),0, Mathf.Sin(angleRad));
-    }
-}
