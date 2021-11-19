7efb0c5b157d5ef6c4c387c63f56c71f668a2abb
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-11-19 03:45:43 -0300

    PauseMenu fix | Level1 y 2 con ScreenManager completo

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -4,34 +4,6 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject  pausePanel;
-    private bool        isPaused;
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            TogglePause();
-        }
-        
-    }
-
-    public void TogglePause()
-    {
-        isPaused = !isPaused;
-
-        if (isPaused)
-        {
-            Time.timeScale = 0f;
-        }
-        else
-        {
-            Time.timeScale = 1f;
-        }
-        pausePanel.SetActive(isPaused);
-    }
-
     public void PauseGame()
     {
         var screenPause = Instantiate(Resources.Load<ScreenPause>("PauseCanvas"));
