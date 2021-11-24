31634c7f4c189397510761b0ce5bde8912c204c1
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-11-24 17:29:12 -0300

    Advertisements

---
+++ Assets/Scripts/AdTypes.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AdTypes
+{
+    Interstitial_Android,
+    Rewarded_Android,
+    Banner_Android
+}
---
+++ Assets/Scripts/Ads.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Ads : MonoBehaviour
+{
+    [SerializeField]
+    private AdTypes adTypes;
+    private string adTypeName;
+    void Start()
+    {
+        #if UNITY_EDITOR
+        Advertisement.Initialize("4470972", true);
+        #else
+        Advertisement.Initialize("4470972", false);
+        #endif
+        adTypeName = adTypes.ToString();
+        StartCoroutine(WaitForAd());
+    }
+
+    private IEnumerator WaitForAd()
+    {
+        while (!Advertisement.IsReady(adTypeName))
+        {
+            yield return null;
+        }
+
+        ShowOptions options = new ShowOptions { resultCallback = AdFinished };
+
+        Advertisement.Show(adTypeName, options);
+    }
+
+    void AdFinished(ShowResult result)
+    {
+        Debug.Log(result.ToString());
+
+        if (result == ShowResult.Finished)
+        {
+            //Rewards
+        }
+    }
+}
