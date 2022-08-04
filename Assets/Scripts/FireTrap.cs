ac1a0d25340d1afb81be46599bead9cf13581573
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2022-08-04 16:09:35 -0300

    FireTrap

--- Assets/Scripts/FireTrap.cs
+++ Assets/Scripts/FireTrap.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class FireTrap : MonoBehaviour
 {
-    [SerializeField] private GameObject _fireParticles;
+    [SerializeField] private ParticleDetector _fireParticles;
+    [SerializeField] private Transform _shotPoint;
+    [SerializeField] private float _particleDuration;
+    private float _timePassed;
+    [SerializeField] private float _shootTime;
+    
+    void Update()
+    {
+        if(_timePassed > 0)
+        {
+            _timePassed -= Time.deltaTime;
+        }
+        else
+        {
+            Instantiate(_fireParticles, _shotPoint.position, Quaternion.identity)
+            .SetDuration(_particleDuration)
+            .SetRotation(transform.forward);
+            
+            _timePassed = _shootTime;
+        }
+
+    }
 }
--- Assets/Scripts/Player/ParticleDetector.cs
+++ Assets/Scripts/Player/ParticleDetector.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class ParticleDetector : MonoBehaviour, IObservable
 {
-    [SerializeField]
     private CanvasManager canvasManager;
     IObserver _myObserver;
     private bool _playerLost;
+    private float _myParticleDuration;
+    private ParticleSystem _myParticleSystem;
 
     void Start()
     {
+        canvasManager = GameObject.Find("CanvasManager").GetComponent<CanvasManager>();
         Subscribe(canvasManager);
+        
+        _myParticleSystem = GetComponent<ParticleSystem>();
+        _myParticleSystem.Stop();
+
+        var main = _myParticleSystem.main;
+        main.duration = _myParticleDuration;
+
+        _myParticleSystem.Play();
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<ParticleSystem>().Stop();
+            var psmain = transform.GetChild(i).GetComponent<ParticleSystem>().main;
+            psmain.duration = _myParticleDuration;
+            transform.GetChild(i).GetComponent<ParticleSystem>().Play();
+        }
     }
 
     void OnParticleCollision(GameObject other)
@@ -42,5 +60,17 @@ public class ParticleDetector : MonoBehaviour, IObservable
         {
             _myObserver = null;
         }
+    }
+
+    public ParticleDetector SetDuration(float seconds)
+    {
+        _myParticleDuration = seconds;
+        return this;
+    }
+
+    public ParticleDetector SetRotation(Vector3 dir)
+    {
+        transform.forward = dir;
+        return this;
     }
 }
