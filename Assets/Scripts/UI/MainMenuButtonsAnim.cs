bd78c740b3e17a971deb4ade7f52cbf1fcdcfac8
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2023-01-21 20:16:34 -0300

    MainMenuButtonsAnim

---
+++ Assets/Scripts/UI/MainMenuButtonsAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Linq;
+
+public class MainMenuButtonsAnim : MonoBehaviour
+{
+    private Button[] _buttons;
+    void Start()
+    {
+       _buttons = GetComponentsInChildren<Button>();
+    }
+
+    public void ActivateButtons() //Se activa con evento en la animacion inicial.
+    {
+        foreach(Button button in _buttons)
+        {
+            button.interactable = true;
+        }
+    }
+}
