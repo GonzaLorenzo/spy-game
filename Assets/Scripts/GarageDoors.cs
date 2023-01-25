2d5242a2bd62df238ab07971d3c5451a24ec599a
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2023-01-25 00:28:26 -0300

    TimelinePart3

---
+++ Assets/Scripts/GarageDoors.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarageDoors : MonoBehaviour
+{
+    private Animator _myAnimator;
+
+    void Start()
+    {
+        _myAnimator = GetComponent<Animator>();    
+    }
+
+    public void OpenDoors()
+    {
+        _myAnimator.SetBool("IsOpen", true);
+    }
+}
