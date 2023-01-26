3c19e756853541d83f3bdd578e8b024b2d45e5dc
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2023-01-26 19:30:54 -0300

    GarageDoor OcclusionPortal

--- Assets/Scripts/GarageDoors.cs
+++ Assets/Scripts/GarageDoors.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class GarageDoors : MonoBehaviour
 {
     private Animator _myAnimator;
+    private OcclusionPortal[] _myOcclusionPortals;
 
     void Start()
     {
         _myAnimator = GetComponent<Animator>();    
+        _myOcclusionPortals = GetComponentsInChildren<OcclusionPortal>();
     }
 
     public void OpenDoors()
     {
+        foreach (OcclusionPortal portal in _myOcclusionPortals)
+        {
+            portal.open = true;
+        }
         _myAnimator.SetBool("IsOpen", true);
     }
 }
