f41df1001d9eafd2d59a4b1a319082c6ef5f7cc5
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2023-01-29 20:59:48 -0300

    TutorialPart1

---
+++ Assets/Scripts/Tutorial/TutorialCollider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialCollider : MonoBehaviour
+{
+    [SerializeField] private TutorialManager _manager;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //_manager.
+    }
+}
---
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    private Animator _myAnimator;
+    private Button _actionButton;
+
+    void Start()
+    {   
+        _myAnimator = GetComponent<Animator>();
+        StartMovementTutorial();
+    }
+
+    public void StartMovementTutorial()
+    {
+        
+    }
+
+    public void EndMovementTutorial()
+    {
+        
+    }
+
+    public void StartShootingTutorial()
+    {
+
+    }
+
+    public void EndShootingTutorial()
+    {
+
+    }
+}
