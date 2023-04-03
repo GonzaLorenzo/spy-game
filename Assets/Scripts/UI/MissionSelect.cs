17cdbf9dc58803e39c7afaf2de22e311005bd25c
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2023-04-03 15:44:04 -0300

    Mission Select FINAL

---
+++ Assets/Scripts/UI/MissionSelect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionSelect : MonoBehaviour
+{
+    [SerializeField] private Button[] myButtons;
+
+    void OnEnable()
+    {
+        for (int i = 0; i < MainMenu.completedLevels + 1; i++)
+        { 
+            myButtons[i].interactable = true;
+            Debug.Log(myButtons[i]);   
+        }
+    }
+}
